1d8ad5557c9ce291831ac12b8bad0e9a71327aa3
SathiyadeviR <sathiya28598@gmail.com>
2025-11-15 17:40:27 +0530

    updated code

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # hello.py
 def greet():
-    return "Hello, Its Suresh Sathiya Sendhan here!"
+    return "Hello, Its Suresh Sathiya Sendhan Siara here!"
 
 if __name__ == "__main__":
     print(greet())
